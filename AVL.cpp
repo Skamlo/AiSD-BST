7974e431bd99277c86db50ece266de58bae93e6f
OgrodowskiJedrzej <jedrekogro@gmail.com>
2024-04-13 14:59:04 +0200

    Rotation for AVL

--- AVL.cpp
+++ AVL.cpp
@@ -41,3 +41,60 @@ void updateHeight(Node *node)
 {
     node->height = 1 + std::max(getNodeHeight(node->left), getNodeHeight(node->right));
 }
+
+Node *rightRotation(Node *node)
+{
+    Node *newroot = node->left;
+    node->left = newroot->right;
+    newroot->right = node;
+
+    updateHeight(node);
+    updateHeight(newroot);
+
+    return newroot;
+}
+
+Node *leftRotation(Node *node)
+{
+    Node *newroot = node->right;
+    node->right = newroot->left;
+    newroot->left = node;
+
+    updateHeight(node);
+    updateHeight(newroot);
+
+    return newroot;
+}
+
+void inorder(Node *root)
+{
+    if (root = nullptr)
+    {
+        return;
+    }
+    inorder(root->left);
+    std::cout << root->key << " ";
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root = nullptr)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    std::cout << root->key << " ";
+}
+
+void preorder(Node *root)
+{
+    if (root = nullptr)
+    {
+        return;
+    }
+    std::cout << root->key << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
