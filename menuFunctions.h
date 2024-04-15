ab57c43623b75e7a5636de15f5ce5b7293b8c895
OgrodowskiJedrzej <jedrekogro@gmail.com>
2024-04-15 17:31:41 +0200

    Deleted duplicates

--- menuFunctions.h
+++
@@ -1,9 +0,0 @@
-# pragma once
-
-#include "BST.h"
-#include "AVL.h"
-
-void findMinMaxBST(NodeBST *node);
-void findMinMaxAVL(NodeAVL *node);
-void printBST(NodeBST *root);
-void printAVL(NodeAVL *root);
